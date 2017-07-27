903b8f7eec2577760a3098ceb38fe55210bb95b4
voidn0ise <ksixevolutionx@hotmail.com>
2017-07-27 08:33:52 -0400

    Further fix for class req.

--- pscan.py
+++ pscan.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import socket, threading, subprocess, json, codecs, sys
+import socket, threading, subprocess, json, codecs, sys, os
 from datetime import datetime
 from urllib.request import urlopen
 
 activeIP = []
+for i in range(240,243):
+    for x in range(1,254):
+        activeIP.append("192.168." + str(i) + "." + str(x))
 
 # Initialize Sockets
 def TCPconnect (ip, portNum, delay, output):
@@ -17,66 +20,37 @@ def TCPconnect (ip, portNum, delay, output):
         output[portNum] = ''
 
 
-def portscan(delay2, time):
-    for hostIP in activeIP:
-        print("Port Scan: " + hostIP + " - [" + str(time) + "]")
-        threads = []
-        output = {}
+def portscan(hostIP, delay):
+    threads = []
+    output = {}
 
-        for i in range(1025):
-            t = threading.Thread(target=TCPconnect, args=(hostIP, i, delay2, output))
-            threads.append(t)
+    for i in range(1025):
+        t = threading.Thread(target=TCPconnect, args=(hostIP, i, delay, output))
+        threads.append(t)
 
-        for i in range(1025):
-            threads[i].start()
+    for i in range(1025):
+        threads[i].start()
 
-        for i in range(1025):
-            threads[i].join()
+    for i in range(1025):
+        threads[i].join()
 
-        for i in range(1025):
-            if output[i] == 'Listening':
-                print("     " + str(i) + ": " + output[i])
-
-
-def ipscan(delay1, time):
-    ipsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ipsock.settimeout(delay1)
-    ipsock.connect(("8.8.8.8", 80))
-    scanip = (ipsock.getsockname()[0])
-    activeIP.append(scanip)
-    scanip = (".".join(scanip.split('.')[0:-2]) + '.')
-    print(str(time) + " - Scanning [" + scanip + "x.x] subnet  for living hosts..")
-    for x in range(240,243):
-        for i in range(1,254):
-            try:
-                ip = scanip + str(x) + "." + str(i)
-                socket.gethostbyaddr(ip)
-                activeIP.append(ip)
-            except socket.herror as ex:
-                pass
-    print("Active IP's:")
-    for i in activeIP:
-        print(i)
-    prompt = input("Continue with scan? (Y/N): ")
-    if prompt == "Y" or "y":
-        ipsock.close()
-        portscan(delay1, time)
-    else:
-        sys.exit()
-
+    for i in range(1025):
+        if output[i] == 'Listening':
+            print("     " + str(i) + ": " + output[i])
 
 def main():
     # Clear Screen
     subprocess.call('clear', shell=True)
 
-    # Obtain External IP
+    # Obtain External IP If needed
     currentIP = json.loads(urlopen("http://ip.jsontest.com/").read().decode('utf-8'))
     currentIP = currentIP["ip"]
 
     # Begin Scan
-    delay = int(input("Enter time (in seconds) for socket timeout: "))
-    t1 = datetime.now()
-    ipscan(delay, t1)
+    delay = float(input("Enter time (in seconds) for socket timeout: "))
+    print("Scanning IP range [192.168.x.x] - " + str(datetime.now()))
+    for i in activeIP:
+        portscan(i, delay)
 
 
 if __name__ == '__main__':
